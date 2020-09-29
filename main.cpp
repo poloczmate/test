ddd91a55f1cbb203a6752333429bdd95de215a72
poloczmate <48856366+poloczmate@users.noreply.github.com>
2020-09-29 10:23:25 +0200

    deleted namespace

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-using namespace std;
 
 int main(){
     int a,b;
-    cin >> a >> b;
+    std::cin >> a >> b;
     int lko = 1;
     int& min = (a < b)?a:b;
     for(int i = 1; i <= min; i++){
@@ -11,6 +10,6 @@ int main(){
             lko = i;
         }
     }
-    cout << lko << endl;
+    std::cout << lko << std::endl;
     return 0;
 }
