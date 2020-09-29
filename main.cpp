8568b9f6d19bd29365fd6bcfae37dd089891a3ca
poloczmate <48856366+poloczmate@users.noreply.github.com>
2020-09-29 10:11:21 +0200

    really fix GDM

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(){
     cin >> a >> b;
     int lko = 1;
     int& min = (a < b)?a:b;
-    for(int i = 1; i < min; i++){
+    for(int i = 1; i <= min; i++){
         if(a%i ==0 && b % i == 0){
             lko = i;
         }
